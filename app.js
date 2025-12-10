e2a7d67e6c279b8ec8423401584399cf9509d29e
Michaiah Campbell <michaiah@campbell-web.com>
2025-12-10 13:38:10 -0500

    added exercise 4 and 5

--- app.js
+++ app.js
@@ -52,3 +52,41 @@ do{
     i++; 
 }
 while(i < 100);
+
+console.log("---------");
+
+
+let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+
+for(let i = 0; i <= n; i++){
+    if(i == value){
+        console.log("Found Value!")
+        break;
+    }
+    else if(i == n){
+        console.log("Did not find value")
+    } 
+}
+
+console.log("-------------")
+
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let m = Math.round(Math.random() * (1000 - 1) + 1);
+let start = Math.round(Math.random() * (10 - 1) + 1);
+
+for(let i = start; i <= m; i++){
+  if(i % fizzDivisor == 0 && i % buzzDivisor == 0){
+        console.log("FizzBuzz");
+    }
+    else if(i % fizzDivisor == 0){
+        console.log("Fizz");
+    }
+    else(i % buzzDivisor == 0)
+    {
+        console.log("Buzz");
+    }
+}
+
+
